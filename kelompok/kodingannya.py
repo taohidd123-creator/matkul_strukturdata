b12af1bd107f4a7c402344aa545783175ad4c3d4
taohidd123-creator <taohidd123@gmail.com>
2026-06-11 13:25:03 +0700

    Refactor graph management and enhance removal logic
    Refactor methods for managing relationships and weights in the graph. Enhance the 'hapus_kebutuhan' method to handle dependencies on 'Dana Bulanan'.

--- kelompok/kodingannya.py
+++ kelompok/kodingannya.py
@@ -1,4 +1,3 @@
-# AABDUSS SYAKUR INI YACH
 # membuat logic untuk visusalisasi streamlit tentang kebutuhan sehari-hari menggunakan graf
 import networkx as nx
 
@@ -6,7 +5,6 @@ import networkx as nx
 class KebutuhanSehariHari:
     # init untuk membuat graph kosong
     def __init__(self):
-        
         self.graph = nx.Graph()
 
     # method untuk menambahkan kebutuhan ke dalam graph
@@ -17,21 +15,26 @@ class KebutuhanSehariHari:
             return True
         return False
 
+# method untuk menambahkan hubungan antara kebutuhan dengan bobot tertentu
     def add_hubungan(self, kebutuhan1, kebutuhan2, bobot):
         if self.graph.has_node(kebutuhan1) and self.graph.has_node(kebutuhan2):
-            self.graph.add_edge(kebutuhan1, kebutuhan2, weight=bobot)
+            if self.graph.has_edge(kebutuhan1, kebutuhan2):
+                current_weight = self.graph.edges[kebutuhan1, kebutuhan2].get('weight', 0)
+                self.graph.edges[kebutuhan1, kebutuhan2]['weight'] = current_weight + bobot
+            else:
+                self.graph.add_edge(kebutuhan1, kebutuhan2, weight=bobot)
             return True
         return False
         
     # fungsi untuk mendapatkan graph kebutuhan
     def get_kebutuhan(self):
-        return self.graph
+        return self.graph 
     
-    # fungsi untuk mendapatkan semua kebutuhan
+    # fungsi untuk mendapatkan semua kebutuhan dalam graf
     def get_all_kebutuhan(self):
         return self.graph.nodes()
     
-    # fungsi untuk mendapatkan semua hubungan
+    # fungsi untuk mendapatkan semua hubungan dengan bobot dalam graf
     def get_all_hubungan(self):
         return self.graph.edges()
     
@@ -41,10 +44,39 @@ class KebutuhanSehariHari:
     
     # fungsi untuk mendapatkan kebutuhan dengan bobot tertentu
     def get_hubungan_dengan_bobot(self):
+        
         return self.graph.edges(data=True)
     # fungsi untuk menghapus kebutuhan dari graf
     def hapus_kebutuhan(self, kebutuhan):
-        if kebutuhan in self.graph:
-            self.graph.remove_node(kebutuhan)
-            return True
-        return False
+        if kebutuhan not in self.graph:
+            return False
+
+        tanggal_node = None
+        kebutuhan_weight = 0
+
+        for neighbor in self.graph.neighbors(kebutuhan):
+            if "Tgl " in str(neighbor):
+                tanggal_node = neighbor
+                kebutuhan_weight = self.graph.edges[kebutuhan, neighbor].get('weight', 0)
+                break
+
+        if tanggal_node is not None and self.graph.has_edge("Dana Bulanan", tanggal_node):
+            remaining_kebutuhan = [
+                node for node in self.graph.neighbors(tanggal_node)
+                if node != "Dana Bulanan" and node != kebutuhan
+            ]
+
+            current_weight = self.graph.edges["Dana Bulanan", tanggal_node].get('weight', 0)
+            new_weight = max(0, current_weight - kebutuhan_weight)
+
+            if not remaining_kebutuhan or new_weight == 0:
+                self.graph.remove_edge("Dana Bulanan", tanggal_node)
+            else:
+                self.graph.edges["Dana Bulanan", tanggal_node]['weight'] = new_weight
+
+        self.graph.remove_node(kebutuhan)
+
+        if tanggal_node is not None and tanggal_node in self.graph and self.graph.degree[tanggal_node] == 0:
+            self.graph.remove_node(tanggal_node)
+
+        return True
